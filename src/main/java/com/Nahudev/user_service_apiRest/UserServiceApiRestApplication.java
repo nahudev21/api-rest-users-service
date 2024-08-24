a06d9fb13486b3b07e03720289f8ef02c03bf3f8
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2024-08-23 22:20:35 -0300

    Registro del servicio en Eureka Server

--- src/main/java/com/Nahudev/user_service_apiRest/UserServiceApiRestApplication.java
+++ src/main/java/com/Nahudev/user_service_apiRest/UserServiceApiRestApplication.java
@@ -3,8 +3,10 @@ package com.Nahudev.user_service_apiRest;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApiRestApplication {
 
